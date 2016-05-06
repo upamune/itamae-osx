bde5c1a4323ef21f6432b6a2dd44b2afc2a5daa8
upamune <jajkeqos@gmail.com>
2016-05-06 22:39:10 +0900

    fix testing

--- spec/localhost/gui_spec.rb
+++ spec/localhost/gui_spec.rb
@@ -5,8 +5,10 @@ describe command "brew tap | grep -q 'caskroom/cask'" do
   its (:exit_status) {should eq 0}
 end
 
+ENV['HOMEBREW_CASK_OPTS']="--appdir=#{ENV['HOME']}/Applications"
+
 Common::GuiPackages.each do |pkg| 
-  describe command "brew cask list | grep -q '#{pkg}'" do
+  describe command "brew cask list '#{pkg}' | grep -q '#{pkg}'" do
     its (:exit_status) {should eq 0}
   end
 end
